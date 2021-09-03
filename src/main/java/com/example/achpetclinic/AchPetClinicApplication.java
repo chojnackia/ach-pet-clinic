6e22c980bf8baa101d7974442633f6ad49b494f8
chojnackia <56468528+chojnackia@users.noreply.github.com>
2021-09-03 06:22:10 +0200

    Initial Project Add

---
+++ src/main/java/com/example/achpetclinic/AchPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.achpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/achpetclinic/AchPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.achpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
