4eb576627c34f6e3f25543e6021d33c33611f29a
chojnackia <56468528+chojnackia@users.noreply.github.com>
2021-09-06 15:29:46 +0200

    Refractoring to common crud interface. Closes #10.

--- pet-clinic-data/src/main/java/com/example/achpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/example/achpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.example.achpetclinic.services;
 
 import com.example.achpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
