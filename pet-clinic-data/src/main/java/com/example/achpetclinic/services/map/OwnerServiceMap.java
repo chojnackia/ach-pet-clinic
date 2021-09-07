d3274c47204e843cf82678b1faa4f0b4f9cef77c
chojnackia <56468528+chojnackia@users.noreply.github.com>
2021-09-07 06:24:57 +0200

    Impl Map based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/example/achpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.example.achpetclinic.services.map;
+
+import com.example.achpetclinic.model.Owner;
+import com.example.achpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
