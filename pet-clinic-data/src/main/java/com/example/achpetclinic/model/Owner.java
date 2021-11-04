f8b0740cead1d89e4bd1417e41b5497870ce87f1
chojnackia <56468528+chojnackia@users.noreply.github.com>
2021-11-04 19:10:14 +0100

    Added properties . Closes #28

--- pet-clinic-data/src/main/java/com/example/achpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/achpetclinic/model/Owner.java
@@ -4,7 +4,37 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String adrdress;
+
+    private String city;
+
+    private String telephone;
+
     private Set<Pet> pets;
+
+    public String getAdrdress() {
+        return adrdress;
+    }
+
+    public void setAdrdress(String adrdress) {
+        this.adrdress = adrdress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
