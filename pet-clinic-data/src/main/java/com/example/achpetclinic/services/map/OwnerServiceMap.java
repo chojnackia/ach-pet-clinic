4576fc302731e6177901aa55dfbed694fbd33294
chojnackia <56468528+chojnackia@users.noreply.github.com>
2021-11-06 14:04:26 +0100

    Fixed Broken Links. Closes #45

--- pet-clinic-data/src/main/java/com/example/achpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/achpetclinic/services/map/OwnerServiceMap.java
@@ -33,16 +33,17 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
         if (object != null) {
             if (object.getPets() != null) {
                 object.getPets().forEach(pet -> {
-                    if (pet.getPetType().getId() != null) {
+                    if (pet.getPetType() != null) {
                         if (pet.getPetType().getId() == null) {
                             pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
                         } else {
                             throw new RuntimeException("Pet Type is required");
                         }
                         if (pet.getId() == null) {
                             Pet savedPet = petService.save(pet);
                             pet.setId(savedPet.getId());
-                        }
+
                     }
                 });
             }
--- pet-clinic-web/src/main/java/com/example/achpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/achpetclinic/bootstrap/DataLoader.java
@@ -32,6 +32,7 @@ public class DataLoader implements CommandLineRunner {
         if (count == 0) {
             loadData();
         }
+
     }
 
     private void loadData() {
