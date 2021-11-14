9277b0f24c1ab566b039b5fda57044b9eb7a5a95
chojnackia <56468528+chojnackia@users.noreply.github.com>
2021-11-14 09:48:22 +0100

    Updated owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/com/example/achpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/achpetclinic/model/Owner.java
@@ -1,16 +1,23 @@
 package com.example.achpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "OWNERS")
 public class Owner extends Person {
 
+    @Column(name = "ADDRESS")
     private String address;
 
+    @Column(name = "CITY")
     private String city;
 
+    @Column(name = "TELEPHONE")
     private String telephone;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAdrdress() {
--- pet-clinic-data/src/main/java/com/example/achpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/achpetclinic/model/Pet.java
@@ -5,17 +5,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "PETS")
 public class Pet extends BaseEntity {
 
+    @Column(name = "NAME")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "BIRTH_DATE")
     private LocalDate birthDate;
 
 }
